c8ff71a4bbaba95c51c623d1d9eef034db1efc54
Morozov Gleb <dt-29@mail.ru>
2019-09-05 20:41:49 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,12 @@
 $(document).ready(function(){
 	$('#nav1').click(function () {
 		$('#nav2').toggleClass('nav-active');
+		$('#spmenu').toggleClass('change');
+		$('#spmenu1').toggleClass('change1');
+		$('#spmenu2').toggleClass('change2');
 		});
 	});
+
+/* $('#nav1').click(function(){
+	$('#spmenu').toggleClass('change');
+}); */
